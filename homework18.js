46c6bb7db5c1fcd0a14d80743c034490362f85b5
KnarikPahlevanyan <knarikpahlevanyan@gmail.com>
2022-07-19 16:57:16 +0400

    homework

---
+++ homework18.js
@@ -0,0 +1,21 @@
+// const sum =function(num1){
+//     return function(num2){
+//         return num1+num2;
+//     }
+// }
+// const sum1 =sum(2);
+// console.log(sum1(3));
+// console.log(sum1(4));
+
+
+//lexical environment
+
+// const name = "Anna";
+// function info(){
+//     let age =20;
+//     return function (){
+     
+//         console.log(`${name} ${age}`);
+//     }
+// }
+// info()();
